7a42fa7f73802224b845b6d733f6f4d558c7869d
Fris <22847866+OGFris@users.noreply.github.com>
2018-07-24 01:25:07 +0100

    Update comments formating

--- GoStats.go
+++ GoStats.go
@@ -1,18 +1,24 @@
-/**
-* MIT License
-*
-* Copyright (c) 2018 Fris
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in all
-* copies or substantial portions of the Software.
-*/
+// MIT License
+//
+// Copyright (c) 2018 Fris
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 package GoStats
 
@@ -26,12 +32,12 @@ const (
 	MinFloat64 = -(MaxFloat64)
 )
 
-//Get the Mean of the float64 numbers.
+// Get the Mean of the float64 numbers.
 func Mean(numbers []float64) float64 {
 	return Total(numbers) / float64(len(numbers))
 }
 
-//Get the Mode of the float64 numbers.
+// Get the Mode of the float64 numbers.
 func Mode(numbers []float64) (mode float64, r int) {
 	r = math.MinInt64
 	for i,n := range Repeats(numbers) {
@@ -43,7 +49,7 @@ func Mode(numbers []float64) (mode float64, r int) {
 	return
 }
 
-//Get the Repeats of the float64 numbers.
+// Get the Repeats of the float64 numbers.
 func Repeats(numbers []float64) (r map[float64]int) {
 	r = make(map[float64]int)
 	for _,n := range numbers {
@@ -52,7 +58,7 @@ func Repeats(numbers []float64) (r map[float64]int) {
 	return
 }
 
-//Get the Total of the float64 numbers.
+// Get the Total of the float64 numbers.
 func Total(numbers []float64) (total float64) {
 	for _,n := range numbers {
 		total += n
@@ -60,7 +66,7 @@ func Total(numbers []float64) (total float64) {
 	return
 }
 
-//Get the Median of the float64 numbers.
+// Get the Median of the float64 numbers.
 func Median(numbers []float64, sort bool) float64 {
 	if sort {
 		numbers = Sort(numbers)
@@ -75,13 +81,13 @@ func Median(numbers []float64, sort bool) float64 {
 	}
 }
 
-//Get whether a number is singular or not.
+// Get whether a number is singular or not.
 func Singular(number float64) bool {
 	n := number / 2
 	return float64(int(n)) != n
 }
 
-//Get the smallest number of the float64 numbers.
+// Get the smallest number of the float64 numbers.
 func Min(numbers []float64) (iMin int,min float64) {
 	min = MaxFloat64
 	for i,n := range numbers {
@@ -93,7 +99,7 @@ func Min(numbers []float64) (iMin int,min float64) {
 	return
 }
 
-//Get the biggest number of the float64 numbers.
+// Get the biggest number of the float64 numbers.
 func Max(numbers []float64) (iMax int, max float64) {
 	max = MinFloat64
 	for i,n := range numbers {
@@ -105,14 +111,14 @@ func Max(numbers []float64) (iMax int, max float64) {
 	return
 }
 
-//Get the Range of the float64 numbers.
+// Get the Range of the float64 numbers.
 func Range(numbers []float64) float64 {
 	_,max := Max(numbers)
 	_,min := Min(numbers)
 	return max - min
 }
 
-//Get the Variance of the float64 numbers.
+// Get the Variance of the float64 numbers.
 func Variance(numbers []float64) (variance float64) {
 	mean := Mean(numbers)
 	for _,n := range numbers {
@@ -123,12 +129,12 @@ func Variance(numbers []float64) (variance float64) {
 	return
 }
 
-//Get the Standard Deviation of the float64 numbers.
+// Get the Standard Deviation of the float64 numbers.
 func StandardDeviation(numbers []float64) float64 {
 	return math.Sqrt(Variance(numbers))
 }
 
-//Get the sorted []float64.
+// Get the sorted []float64.
 func Sort(numbers []float64) []float64 {
 	sort.Float64s(numbers)
 	return numbers
